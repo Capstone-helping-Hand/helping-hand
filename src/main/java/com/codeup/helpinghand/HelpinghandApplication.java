bb2b52eccdec7325fc1a5289274ec6b8049861cb
lening rodas <lening.a.rodas@gmail.com>
2021-03-08 09:59:14 -0600

    test

---
+++ src/main/java/com/codeup/helpinghand/HelpinghandApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.helpinghand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpinghandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpinghandApplication.class, args);
+    }
+
+}
