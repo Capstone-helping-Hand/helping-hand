a2563ec627cafa4e57055616c6fe6125c914dc5f
lening rodas <lening.a.rodas@gmail.com>
2021-03-16 15:40:09 -0500

    reqorm linked into, request controller changed, user controller for login need works

--- src/main/java/com/codeup/helpinghand/controllers/HomeController.java
+++ src/main/java/com/codeup/helpinghand/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.codeup.helpinghand.controllers;
 
 import com.codeup.helpinghand.repositories.UserRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -16,7 +20,8 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
         return "index";
     }
 
