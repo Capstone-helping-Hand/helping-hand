d3aa1f19dfaedfeff710c409351220b443e0335c
Christian Parker <christianparker512@gmail.com>
2021-03-17 13:47:06 -0500

    displays last five donations, last five requests, last five pending donations and last five pending requests on the admindashboard

--- src/main/java/com/codeup/helpinghand/repositories/DonationRepository.java
+++ src/main/java/com/codeup/helpinghand/repositories/DonationRepository.java
@@ -2,7 +2,14 @@ package com.codeup.helpinghand.repositories;
 
 import com.codeup.helpinghand.models.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+//Same thing below for requests
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+    @Query(value = "SELECT * FROM donations WHERE is_approved = true ORDER BY date DESC LIMIT 5", nativeQuery = true)
+    List<Donation>lastFive();
 
+    @Query(value = "SELECT * FROM donations WHERE is_approved = false ORDER BY date DESC LIMIT 5", nativeQuery = true)
+    List<Donation>lastFivePending();
 }
