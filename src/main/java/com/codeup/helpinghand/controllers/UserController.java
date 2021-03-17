30de86531f21f6c032fb6b503a6be2bb8425ae65
Christian Parker <christianparker512@gmail.com>
2021-03-17 09:40:35 -0500

    sass, footer, and commented out user controller, user model, and user repository

--- src/main/java/com/codeup/helpinghand/controllers/UserController.java
+++ src/main/java/com/codeup/helpinghand/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.codeup.helpinghand.models.User;
 import com.codeup.helpinghand.repositories.RequestRepository;
 import com.codeup.helpinghand.repositories.UserRepository;
 import com.codeup.helpinghand.services.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -67,7 +68,11 @@ public class UserController{
         userDao.save(user);
         return "redirect:/login";
     }
-
+//    @GetMapping("/userdashboard")
+//    public String lastfive(Model model){
+//        User user = (User) SecurityContextHolder.GetContext().getAuthentication().getPrincipal();
+//        model.addAttribute()
+//    }
 
 
 }
