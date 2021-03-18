52ac596817fd09ab56bf681d14e8958ed254e66f
Christian Parker <christianparker512@gmail.com>
2021-03-18 13:38:19 -0500

    morning changes

--- src/main/java/com/codeup/helpinghand/repositories/UserRepository.java
+++ src/main/java/com/codeup/helpinghand/repositories/UserRepository.java
@@ -1,19 +1,8 @@
 package com.codeup.helpinghand.repositories;
 
-import com.codeup.helpinghand.models.Donation;
 import com.codeup.helpinghand.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-
-//    private DonationRepository donationDao;
-//    @Override
-//    public List<Donation> findLastFiveDonations() {
-//        return donationDao.createQuery("SELECT * FROM donations WHERE donation_id > (SELECT MAX(donation_id) FROM donations) - 5",
-//                Donation.class).getResultList();
-//    }
 }
-
