f02907fbc36be7247f7536a21e1ac403751d2219
Christian Parker <christianparker512@gmail.com>
2021-03-18 10:07:55 -0500

    all things related to donations

--- src/main/java/com/codeup/helpinghand/repositories/DonationRepository.java
+++ src/main/java/com/codeup/helpinghand/repositories/DonationRepository.java
@@ -2,10 +2,12 @@ package com.codeup.helpinghand.repositories;
 
 import com.codeup.helpinghand.models.Donation;
 import com.codeup.helpinghand.models.Request;
+import com.codeup.helpinghand.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+
 //Same thing below for requests
 public interface DonationRepository extends JpaRepository<Donation, Long> {
     @Query(value = "SELECT * FROM donations WHERE is_approved = true ORDER BY date DESC LIMIT 5", nativeQuery = true)
@@ -15,8 +17,15 @@ public interface DonationRepository extends JpaRepository<Donation, Long> {
     List<Donation>lastFivePending();
 
     @Query(value = "SELECT * from donations WHERE is_approved = true ORDER BY date", nativeQuery = true)
-    List<Request> allDonations();
+    List<Donation> allDonations();
 
     @Query(value = "SELECT * from donations WHERE is_approved = false ORDER BY date", nativeQuery = true)
-    List<Request> allPendingDonations();
+    List<Donation> allPendingDonations();
+
+    @Query(value = "SELECT * from donations WHERE is_approved = true ORDER BY donator_id and date DESC LIMIT 5", nativeQuery = true)
+    List<Donation> lastFiveUserDonations();
+
+    List<Donation> findAllByUserAndIsApproved(User user);
+
+    List<Donation> findAllByUser(User user);
 }
