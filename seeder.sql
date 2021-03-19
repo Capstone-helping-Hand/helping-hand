ee87fb0f0f6b21fc98204e7213b839d5f7cf3028
lening rodas <lening.a.rodas@gmail.com>
2021-03-19 09:37:55 -0500

    category added on radio buttons

--- seeder.sql
+++ seeder.sql
@@ -5,3 +5,9 @@ values (1, 'ADMIN');
 insert into roles (role_id, role)
 values (2, 'USER');
 
+insert into categories(category_id, type)
+values (1, 'indoor');
+insert into categories(category_id, type)
+values (2, 'outdoor');
+insert into categories(category_id, type)
+values(3, 'other');
